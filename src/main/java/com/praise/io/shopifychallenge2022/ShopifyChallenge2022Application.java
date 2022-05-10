8fdb328701ecf8b4b301ff07dce9631ac1e0d6ef
Praise Uadiale <puadiale@gmail.com>
2022-05-10 22:35:54 +0100

    first commit

---
+++ src/main/java/com/praise/io/shopifychallenge2022/ShopifyChallenge2022Application.java
@@ -0,0 +1,13 @@
+package com.praise.io.shopifychallenge2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyChallenge2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifyChallenge2022Application.class, args);
+	}
+
+}
